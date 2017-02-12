c0b150e4b01f581571cb254318de55503d0d5b5e
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-02-12 19:21:19 +0100

    Project list can be shown as CSV

---
+++ getProjectListAsCSV.js
@@ -0,0 +1,26 @@
+/**
+ * Definition of getProjectListAsCSV.js
+ *
+ * @author     joachim.schirrmacher@deutschebahn.com
+ */
+/*global define*/
+'use strict';
+
+requirejs.config({
+    baseUrl: '.',
+    paths: {
+    }
+});
+
+define(['lib/text!data/projects.json'], function (projects) {
+    JSON.parse(projects).nodes.forEach(function (project, index) {
+        if (!index) {
+            document.write(Object.keys(project).join(','));
+        }
+        var row = [];
+        Object.keys(project).forEach(function (key) {
+            row.push('"' + project[key] + '"');
+        });
+        document.write(row.join(','), '<br>');
+    });
+});
