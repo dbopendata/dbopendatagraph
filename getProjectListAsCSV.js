12ebfd89ce80bb5c562223cb3b97d008c8cdde69
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-02-12 22:07:09 +0100

    Added missing line break

--- getProjectListAsCSV.js
+++ getProjectListAsCSV.js
@@ -15,7 +15,7 @@ requirejs.config({
 define(['lib/text!data/projects.json'], function (projects) {
     JSON.parse(projects).nodes.forEach(function (project, index) {
         if (!index) {
-            document.write(Object.keys(project).join(','));
+            document.write(Object.keys(project).join(',') + '<br>');
         }
         var row = [];
         Object.keys(project).forEach(function (key) {
